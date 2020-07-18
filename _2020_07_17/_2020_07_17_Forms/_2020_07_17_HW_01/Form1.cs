7ea603591be0e740c5986e5c0a58b63f7e1a41bc
푸른 정 <ggznzn007@naver.com>
2020-07-18 19:06:32 +0900

    20200718

---
+++ _2020_07_17/_2020_07_17_Forms/_2020_07_17_HW_01/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020_07_17_HW_01
+{
+    public partial class Form1 : Form
+    {
+        const int RAD = 100;
+        const int MOVE_VAL = 10;
+        int x = 10, y = 10;
+    }
+
+    
+    {
+        
+        
+    }
+
+    
+}
+    
+
+
+  
+
